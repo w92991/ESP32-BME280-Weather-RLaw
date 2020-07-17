3ef3b5918e21b8e2091695f306cf0dba9cbe7f1f
robbinl <robbinl@telusplanet.net>
2020-07-17 17:33:26 -0600

    first commit on this branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,31 @@
 #include <Arduino.h>
 #include "sensor_readings.h"
+#include "TFT_eSPI.h"     // ESP32 Hardware-specific library
 #include "settings.h"    // The order is important!
 
 Adafruit_BME280 bme;
+TFT_eSPI tft = TFT_eSPI();
+
+uint16_t bg = TFT_BLACK;
+uint16_t fg = TFT_WHITE;
 
 void setup() {
   pinMode(LED_BUILTIN,OUTPUT);
   Serial.begin(9600);
   bool status;
+  // Setup the TFT
+  
+  tft.begin();
+
+  tft.setRotation(3);
+  tft.setTextColor(fg, bg);
+  
+  tft.fillScreen(bg);
+  
+  tft.setCursor(0, 0);
+  tft.println("Hello!");
+  tft.println("Starting BME sensor...");
+  delay(5000);
 
   // (you can also pass in a Wire library object like &Wire2)
   status = bme.begin(0x76);
@@ -15,6 +33,7 @@ void setup() {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
     while (1);  // Infinite loop
   }
+  tft.fillScreen(bg);
 }
 
 void loop() {
